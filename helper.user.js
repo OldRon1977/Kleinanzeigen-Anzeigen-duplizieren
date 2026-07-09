416e485e0ebc1657ad6f1ac96458a82a5a4d0c1b
OldRon1977 <oldron1977@gmail.com>
2026-07-09 18:14:52 +0200

    fix: non-www @match entfernt - Scripts sind origin-gebunden an www (BUG-005)

--- helper.user.js
+++ helper.user.js
@@ -8,7 +8,6 @@
 // @version       1.3.1
 // @author        panzli (Original), OldRon1977 (Anpassungen)
 // @match         https://www.kleinanzeigen.de/m-meine-anzeigen.html*
-// @match         https://kleinanzeigen.de/m-meine-anzeigen.html*
 // @homepage      https://github.com/OldRon1977/Kleinanzeigen-Anzeigen-duplizieren
 // @updateURL     https://github.com/OldRon1977/Kleinanzeigen-Anzeigen-duplizieren/raw/main/helper.user.js
 // @downloadURL   https://github.com/OldRon1977/Kleinanzeigen-Anzeigen-duplizieren/raw/main/helper.user.js
